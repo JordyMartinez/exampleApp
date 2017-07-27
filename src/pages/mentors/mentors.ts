5c5433adc39cb198b77f2ab07fc426c59505bb67
Vanessa Gutierrez <vanessagutierrez@guest-wireless-upc-1601-10-120-030-167.usc.edu>
2017-07-27 13:23:32 -0700

    fourth commit

--- src/pages/mentors/mentors.ts
+++ src/pages/mentors/mentors.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { ChrisCohenPage } from '../chris-cohen/chris-cohen';
 import { VanessaSlopPage } from '../vanessa-slop/vanessa-slop';
 import { SearchPage } from '../search/search';
+import  { MentorsProvider } from '../../providers/mentors/mentors';
 // import { SteveJobsPage } from '../steve-jobs/steve-jobs';
 // import { SteveWozniakPage } from '../steve-wozniak/steve-wozniak';
 //import { SteveWozniakPage } from '../steve-wozniak/steve-wozniak';
@@ -19,20 +20,21 @@ import { SearchPage } from '../search/search';
   templateUrl: 'mentors.html',
 })
 export class MentorsPage {
-// items = [
-//     {id: 0, title: 'Chris Cohen' },
-//     {id: 1, title: 'Vanssa Slop' },
-//     {id: 2, title: 'Chris Hop'},
-//     {id: 3, title: 'Erika Salmos'}
-
-  // 'Steve Jobs',
-  // 'Steve Wozniak'
-// ];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mentors: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public mentorsService: MentorsProvider, public ModalController: ModalController) {
   }
+
+  ionViewDidLoad(){
+    this.mentorsService.getMentors().then((data) => {
+      console.log(data);
+      this.mentors=data;
+    });
+  }
+
   openChrisCohenPage() {
     this.navCtrl.push(ChrisCohenPage)
   }
+  
 openVanessaSlopPage() {
     this.navCtrl.push(VanessaSlopPage)
 }
--- src/providers/mentors/mentors.ts
+++ src/providers/mentors/mentors.ts
@@ -10,10 +10,34 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class MentorsProvider {
-	data: any;
+  data: any;
   constructor(public http: Http) {
     console.log('Hello MentorsProvider Provider');
     this.data = null;
+  }
+
+  getMentors(){
+  	if (this.data){
+  		return Promise.resolve(this.data);
+  	}
+  	return new Promise(resolve => {
+  		this.http.get('http://localhost:8080/api/mentors')
+  			.map(res => res.json())
+  			.subscribe(data => {
+  				this.data = data;
+  				resolve(this.data);
+  			});
+  	});
+  }
+
+  createMentor(mentor){
+  	let headers = new Headers();
+  	headers.append('Content-Type', 'application/json');
+
+  	this.http.post('http://localhost:8080/api/mentors', JSON.stringify(mentor), {headers: headers})
+  		.subscribe(res => {
+  			console.log(res.json());
+  		});
   }
 
 }
